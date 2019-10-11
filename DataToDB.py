6309e43352a4fcacc070cc5f182781e2478c5041
LiaozhiCheng <51042415+LiaozhiCheng@users.noreply.github.com>
2019-10-11 15:13:40 +0800

    後台接收活動表單(尚未整合)

---
+++ DataToDB.py
@@ -0,0 +1,31 @@
+import flask
+from flask import render_template, request
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://Liao:871029@cluster0-sk2jk.mongodb.net')
+db = client['EventResourse']
+col = db['Event']
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+app.config["JSON_AS_ASCII"] = False
+
+def insert_data(event):
+    if col.find_one({"eventName":event["eventName"]}) is None:
+        col.insert_one(event)
+        print('insert success')
+    else:
+        print(col.find_one({"eventName":event["eventName"]}))
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("elements.html")
+
+@app.route('/complete', methods=['GET'])
+def complete():
+    activity_data = request.values.to_dict()
+    insert_data(activity_data)
+    return 'Hello'+request.values['eventName'] 
+
+
+app.run()
